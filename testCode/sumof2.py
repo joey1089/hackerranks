93e803fae6a1fc678023269705b5e987013a9ba6
Joseph Peter <josephvpeter7@gmail.com>
2023-04-20 00:13:00 -0400

    testcode

---
+++ testCode/sumof2.py
@@ -0,0 +1,9 @@
+#sum of two numbers
+
+def sumof2(no1,no2):
+    if no1 != "" and no2 != "":
+        sum = no1 + no2
+        return sum
+    
+
+print("The sum of two numbers : ",sumof2(983,3434))
