455c5b19a05a7d8005c2674ff542a2f288504a5a
Joseph Peter <josephvpeter7@gmail.com>
2023-04-20 00:50:53 -0400

    basic-br-up

---
+++ testCode/testlist.py
@@ -0,0 +1,14 @@
+# list - operations
+
+# List Iteration (method-1):
+a = ["item_0","item_1","item_2","item_3","item_4"]
+# for item in a:
+#     print(item)
+
+# List Iteration (method-2):
+# range(3) creates a list of [0,1,2]
+for i in range(3):
+    print(i)
+# To get the item from the list
+for i in range(len(a)):
+    print(a[i])
